77b2a6cead83e1928bd0c566c139364df0e0e9e7
David Suárez Garcia <120138460+david-garcia1402@users.noreply.github.com>
2023-12-20 10:50:09 -0300

    finalização das funções que retornarão a response da api (#1)

--- resources/views/user-list.blade.php
+++ resources/views/user-list.blade.php
@@ -15,21 +15,7 @@
             $items = $userData->resource->items();
             $contador = 1;
         @endphp
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                  <a class="nav-link" href="#">Usuários</a>
-                  <a class="nav-link" href="#">Cadastrar</a>
-                  {{-- <a class="nav-link" href="#">Pricing</a>
-                  <a class="nav-link" href="#">Disabled</a> --}}
-                </div>
-              </div>
-            </div>
-          </nav>
+        @include('include.navbar')
         <div class="container">
             <table class="table">
                 <thead>
